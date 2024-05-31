1654fdf672197580206c575d762e8843b8b21624
Adham Ali <30821115+adhamali450@users.noreply.github.com>
2024-05-31 18:46:23 +0300

    fixed a rank bug and added iframe compatibility check

--- scrape_news.py
+++ scrape_news.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 import boto3
+import json
+import urllib3
 import time
 from datetime import datetime, timezone
 
@@ -11,16 +13,43 @@ def valid_news_title(title: str) -> bool:
     return not (title.isupper() or title == '')
 
 def calculate_news_rank():
-    base_unix = 1716888352
+    base_unix = 1717167540
     now_unix = int(time.time())
 
     today_date = datetime.fromtimestamp(base_unix, tz=timezone.utc).date()
     
     scrape_date = datetime.fromtimestamp(now_unix, tz=timezone.utc).date()
     
-    date_diff = (today_date - scrape_date).days
+    date_diff = (scrape_date - today_date).days
     
     return date_diff + 1  # Adding 1 to make the rank 1-based
+
+
+def allow_iframe(url: str) -> bool:
+    if not url:
+        return {
+            'statusCode': 400,
+            'body': json.dumps({'error': 'URL parameter is required'})
+        }
+
+    http = urllib3.PoolManager()
+    try:
+        response = http.request('HEAD', url)
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': str(e)})
+        }
+
+    headers = response.headers
+    x_frame_options = headers.get('X-Frame-Options')
+    content_security_policy = headers.get('Content-Security-Policy')
+
+    can_render_in_iframe = True
+    if x_frame_options or (content_security_policy and 'frame-ancestors' in content_security_policy):
+        can_render_in_iframe = False
+
+    return can_render_in_iframe
 
 
 def scrap(event, context):
@@ -45,13 +74,13 @@ def scrap(event, context):
                     "newsId": str(hash(link["href"])),
                     "newsOriginalTitle": link.text,
                     "newsUrl": link["href"],
-                    "newsContent": "",
+                    "newsContent": "", # Placeholder for content
                     "newsRank": calculate_news_rank(),
-                    "newsNewTitle": "",
+                    "newsNewTitle": "", # When admin updates the title
                     "isScrapeContent": False,
                     "isJokesGenearted": False,
-                    "isRender": True,
-                    "newsImageURL": ""
+                    "isRender": allow_iframe(link["href"]),
+                    "newsImageURL": "" # Added by admin
                 })
                 
         return table.scan()

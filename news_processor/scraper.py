eb935199368039f2ea60685e14d240d379e9827b
Anh Bui <metwaroi31@gmail.com>
2024-07-02 17:39:36 +0700

    hotfix: chrome options for deployment

--- news_processor/scraper.py
+++ news_processor/scraper.py
@@ -14,6 +14,10 @@ class Scraper():
     def __init__(self) -> None:
         self.options = Options()
         self.options.add_argument('--headless=new')
+        self.options.add_argument('--ignore-ssl-errors=yes')
+        self.options.add_argument('--ignore-certificate-errors')
+        user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.517 Safari/537.36'
+        self.options.add_argument('user-agent={0}'.format(user_agent))
 
         self.browser = webdriver.Chrome(options=self.options)
     

d13313893ff149b6a57455451b8cf730e06cbde1
Anh Bui <metwaroi31@gmail.com>
2024-07-09 00:44:19 +0700

    hotfix : return headless

--- news_processor/scraper.py
+++ news_processor/scraper.py
@@ -28,7 +28,7 @@ class Scraper():
     
     def __init__(self) -> None:
         self.options = Options()
-        # self.options.add_argument('--headless=new')
+        self.options.add_argument('--headless=new')
         self.options.add_argument('--ignore-ssl-errors=yes')
         self.options.add_argument('--ignore-certificate-errors')
         user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.517 Safari/537.36'
